ad88e55d97bdce287b76b5713316ff18828f0adf
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-09-28 19:35:51 +0300

    Знакомство

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+console.log("hello pediki");
+let num=7;
+
+num = 25;
+
+console.log(num);
+
+let colorEyes = '#blue';
+let user;
+let userName = 'Вася';
+user = userName;
+console.log (user);
