da26c247f881d13a4379d2aedf326553b5f46c92
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-10-06 14:55:57 +0300

    Операторы, переменные

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict"
-console.log("hello pediki");
+console.log("I learning JS");
 let num=7;
 
 num = 25;
@@ -10,4 +10,43 @@ let colorEyes = '#blue';
 let user;
 let userName = 'Вася';
 user = userName;
-console.log (user);
+console.log(user);
+console.log(userName);
+console.log(colorEyes);
+console.log(num);
+
+let myName = 'Alex';
+let myAge = '36';
+let myWork = 'Marksheider';
+console.log(myWork);
+myAge = 55;
+console.log(myAge);
+
+const myDog = "Daineris";
+console.log(myDog);
+
+
+
+var wakeUpTime = '5.00 in the morning';
+console.log(typeof wakeUpTime);
+
+let trueOrFalse = 55 > 33;
+console.log(trueOrFalse);
+
+
+let userInfo = {
+   name: 'Alex',
+   age: 36
+}
+console.log(userInfo);
+
+let userTrue = true;
+console.log(userTrue);
+console.log(typeof userTrue);
+
+userTrue = String(userTrue);
+console.log(userTrue);
+console.log(typeof userTrue);
+
+let z = 6**2;
+console.log(`[Абсолютная высота будет:${z}]`);
