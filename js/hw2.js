27148b109e8877a1fd04837e606fd5a5789e7bb2
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-10-19 08:38:28 +0300

    modul#2_task#9

--- js/hw2.js
+++ js/hw2.js
@@ -1,22 +1,65 @@
 // #1
-function checkAge(age) {
-  if (age >= 18) { 
-    return "You are an adult";
-  }
-  return "You are a minor";
-}
-console.log(checkAge(36));
+// function checkAge(age) {
+//   if (age >= 18) { 
+//     return "You are an adult";
+//   }
+//   return "You are a minor";
+// }
+// console.log(checkAge(36));
 
 
 // #2
-function checkPassword(password) {
-  const ADMIN_PASSWORD = "Repeta";
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "Repeta";
 
-  if (password === ADMIN_PASSWORD) {
-    return "Welcome!";
-  }  
-    return "Access denied, wrong password!";
-  }
-  console.log(checkPassword('Danilko'));
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }  
+//     return "Access denied, wrong password!";
+//   }
+//   console.log(checkPassword('Danilko'));
 
 //   #3
+// function checkStorage(available, ordered) {
+
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   }  if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+//   return "The order is accepted, our manager will contact you";
+// }
+// console.log(checkStorage(200, 250));
+  
+// #4
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// #5
+// const fruits = ["apple", "plum", "pear", "orange"];
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
+// #6
+// const fruits = ["apple", "plum", "pear", "orange"];
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
+// #7
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const fruitsArrayLength = fruits.length;
+// console.log(fruitsArrayLength);
+
+// #8
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+// console.log(lastElement);
+
+// #9
+function getExtremeElements(array) {
+  const element = getExtremeElements[0];
+  return element;
+}
+console.log([1, 2, 3, 4, 5]);
+// #10
